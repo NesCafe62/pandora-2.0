afd498644a57925b1f70cfd2f0fd79a464a656b4
NesCafe <U.238@rambler.ru>
2017-11-17 00:00:45 +0300

    layout base

--- apps/app/layouts/main.php
+++ apps/app/layouts/main.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<base href="/"/>
+		<base href="<?= $app->uriBase ?>"/>
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" href="css/styles.css"/>
 		<?= $this->head() ?>
